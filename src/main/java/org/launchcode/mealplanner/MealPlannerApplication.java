1e5bb47253bae370d9150ab5d4b3d6291808c6de
jasonramo <jasonaramo@gmail.com>
2019-01-25 15:04:00 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/mealplanner/MealPlannerApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
+
