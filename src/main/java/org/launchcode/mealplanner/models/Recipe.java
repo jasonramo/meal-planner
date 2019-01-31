9557d415e08e43b1eeb6ce4c5c9eb6f6504967ee
jasonramo <jasonaramo@gmail.com>
2019-01-31 15:12:50 -0600

    Added a template for the addition of a recipe and modified my recipe class.

--- src/main/java/org/launchcode/mealplanner/models/Recipe.java
+++ src/main/java/org/launchcode/mealplanner/models/Recipe.java
@@ -1,4 +1,65 @@
 package org.launchcode.mealplanner.models;
 
+import com.sun.istack.internal.NotNull;
+import javax.validation.constraints.Size;
+
 public class Recipe {
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "Description must not be empty")
+    private String description;
+
+    @NotNull
+    @Size(min = 1, message = "Ingredients must not be empty")
+    private String ingredients;
+
+    private int recipeId;
+    private static int nextId = 1;
+
+    public Recipe(String name, String description, String ingredients) {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+    }
+
+    public Recipe() {
+        recipeId = nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
 }
