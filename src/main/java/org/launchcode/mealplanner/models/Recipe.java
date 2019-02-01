e136131c2cd89850a41abdb46930635f2c0f46d2
jasonramo <jasonaramo@gmail.com>
2019-02-01 16:55:50 -0600

    Completed the recipe removal page and updated the add page

--- src/main/java/org/launchcode/mealplanner/models/Recipe.java
+++ src/main/java/org/launchcode/mealplanner/models/Recipe.java
@@ -1,6 +1,6 @@
 package org.launchcode.mealplanner.models;
 
-import com.sun.istack.internal.NotNull;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Recipe {
